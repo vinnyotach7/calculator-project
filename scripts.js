6b3f70d21dea5cb89ebea4f1c50a5c9614a1093a
vinnyotach7 <vinnyotach7@gmail.com>
2018-09-11 10:38:26 +0300

    Add the changes committed

---
+++ scripts.js
@@ -0,0 +1,27 @@
+var add = function(number1, number2){
+return number1 + number2;
+};
+add(1, 2);
+var result = add(1, 2);
+alert(result);
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+alert(subtract(10,5));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = prompt("Enter a number:");
+alert(number1);
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = prompt("Enter a number:");
+var number2 = prompt("Enter another number:");
+alert(add(number1, number2));
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2);
